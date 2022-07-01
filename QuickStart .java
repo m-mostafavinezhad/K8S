ff5110a0c916feea77a541408e08f3ece2f8aab6
m-mostafavinezhad <52644754+m-mostafavinezhad@users.noreply.github.com>
2022-07-02 01:44:35 +0430

    Add files via upload

---
+++ QuickStart .java
@@ -0,0 +1,7 @@
+class QuickStart {
+    public static void main(String[] args) {
+        System.out.println("this is basic java test, World.");
+        System.out.println("Test");
+        System.out.println("Sarv");
+    }
+}
